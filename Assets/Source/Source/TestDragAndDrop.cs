d5512e5b5781ca156ebbed1c2541fcec24ae4b3c
Ivan <gureff.al@yandex.ru>
2024-03-26 16:16:54 +0300

    Add trigger script

--- Assets/Source/Source/TestDragAndDrop.cs
+++ Assets/Source/Source/TestDragAndDrop.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class TestDragAndDrop : MonoBehaviour
 {
+    private Vector2 _basePosition;
+
+    private void Start()
+    {
+        _basePosition = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        //transform.position = _basePosition;
+    }
+
     private void OnMouseDrag()
     {
         Vector3 objectPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
